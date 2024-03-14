34cefee0d70d02542e742abecf4f2f19a91dfd2e
juga <juga@riseup.net>
2024-03-14 17:04:41 +0000

    Add util test_relay_in_consensus unit test
    and pytest fixtures to use data files.

---
+++ test/conftest.py
@@ -0,0 +1,12 @@
+import os.path
+import pytest
+
+
+@pytest.fixture()
+def data_path():
+    return os.path.join(os.path.abspath(os.path.dirname(__file__)), "data")
+
+
+@pytest.fixture()
+def cached_consensus_path(data_path):
+    return os.path.join(data_path, "cached-consensus")
