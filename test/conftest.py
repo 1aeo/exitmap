a3d9b1e5a473ad98320675096d3190b180e7c6c5
juga <juga@riseup.net>
2024-11-29 19:14:04 +0000

    test: Add relayselector tests
    Part of #20

--- test/conftest.py
+++ test/conftest.py
@@ -15,3 +15,13 @@ def cached_consensus_path(data_path):
 @pytest.fixture()
 def cached_descriptors_path(data_path):
     return os.path.join(data_path, "cached-descriptors")
+
+
+@pytest.fixture()
+def cached_consensus(cached_consensus_path):
+    # if imported on the top, the percent coverage of relayselector.py test
+    # will be 0
+    import relayselector
+
+    cc = relayselector.get_cached_consensus(cached_consensus_path)
+    return cc
